e498265aa1deab984060b21d9ac8e979e2e483ad
tmv2000 <ruscrafts@gmail.com>
2022-05-13 23:08:10 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -3,10 +3,9 @@ package ru.job4j;
 public class Multiple {
     public static void main(String[] args) {
         int i;
-
-        for(i = 0; i <=9; i++) {
-            System.out.println("1 * " + i + " = " + (1 * i));
-
+        int n = 1;
+        for(i = 0; i <= 9; i++) {
+            System.out.println("1 * " + i + " = " + n * i);
         }
     }
 }
