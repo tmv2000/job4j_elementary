6e8964313d9c8ea63e56bffd1c20d1c3b500acee
tmv2000 <ruscrafts@gmail.com>
2022-05-13 16:04:48 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru.job4j/Multiple.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        int i;
+
+        for(i = 0; i <=9; i++) {
+            System.out.println("1 * " + i + " = " + (1 * i));
+
+        }
+    }
+}
---
+++ src/main/java/ru.job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+    System.out.println(" Mikhail Terletskiy");
+    System.out.println("05.12.1991");
+    }
+}
