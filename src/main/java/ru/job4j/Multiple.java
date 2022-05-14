53573775b555e8948b87a7a45266bb1c5b23dc46
tmv2000 <ruscrafts@gmail.com>
2022-05-14 10:51:13 +0300

    4.1. Разбор первой программы. [#159033 #300598]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -4,7 +4,7 @@ public class Multiple {
     public static void main(String[] args) {
         int i;
         int n = 1;
-        for(i = 0; i <= 9; i++) {
+        for (i = 0; i <= 9; i++) {
             System.out.println("1 * " + i + " = " + n * i);
         }
     }
